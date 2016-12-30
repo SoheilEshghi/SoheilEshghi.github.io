ebe8976e3e67217d45b9e820d7fd35d3a0460fa9
Soheil <soheil.eshghi@gmail.com>
2016-12-30 11:54:46 -0500

    highlight active section

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
 console.log('This would be the main JS file.');
+
+<script src="JavaScript/jquery-1.10.2.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $(function() {
+        // this will get the full URL at the address bar
+        var url = window.location.href;
+
+        // passes on every "a" tag
+        $(".topmenu a").each(function() {
+            // checks if its the same on the address bar
+            if (url == (this.href)) {
+                $(this).closest("li").addClass("active");
+            }
+        });
+    });
+</script>
